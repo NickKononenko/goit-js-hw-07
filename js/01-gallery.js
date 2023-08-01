75284fa0709747de6bcbfab2da7ad3b5fd0253c7
NickKononenko <kononenko0812@gmail.com>
2023-08-01 15:51:09 +0300

    Final commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+let instance;
+
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) => {
+    return (
+      acc +
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    );
+  },
+  ""
+);
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  instance = basicLightbox.create(`
+ <img class="gallery__image" src='${event.target.dataset.source}'>
+  `);
+  instance.show();
+}
+
+gallery.addEventListener("keydown", (event) => {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) => {
+    return (
+      acc +
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`
+    );
+  },
+  ""
+);
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.9,
+  captionsData: "alt",
+  captionDelay: 250,
+});
